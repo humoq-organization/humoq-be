eb91406e0933c33c62b56bffec88f6b6727f1838
karabatak <oktay.karabatak@gmail.com>
2021-12-14 19:05:18 +0300

    cdn image add

--- src/utils/serializer.js
+++ src/utils/serializer.js
@@ -1,13 +1,16 @@
 import { convertToSlug } from "./helper";
 
+const cdnThumbnail = "https://assets.humoq.com/images/h100/";
+const cdnImage = "https://assets.humoq.com/images/h512/";
+
 export const serializeData = (data) => {
     if(!Array.isArray(data)) {
-        return { title: data?.Title, description: data?.Description, url: data?.Url, slug: convertToSlug(data?.Title), images: data?.Asset }
+        return { title: data?.Title, description: data?.Description, url: data?.Url, slug: convertToSlug(data?.Title), thumbnail: cdnThumbnail + convertToSlug(data?.Title) + ".png", image: cdnImage + convertToSlug(data?.Title) + ".png", images: data?.Asset }
     }
     const newData = [];
     if(data?.length > 0) {
         data.map(val => {
-            newData.push({ title: val?.Title, description: val?.Description, url: val?.Url, slug: convertToSlug(val?.Title), images: val?.Asset })
+            newData.push({ title: val?.Title, description: val?.Description, url: val?.Url, slug: convertToSlug(val?.Title), thumbnail: cdnThumbnail + convertToSlug(val?.Title) + ".png", image: cdnImage + convertToSlug(val?.Title) + ".png", images: val?.Asset })
         })
     }
     return newData;
